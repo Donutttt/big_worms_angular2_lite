9e38ae5f1c3761f1a4cd67f0deb68f7b0be9c6e4
Oliver <orr2@live.com>
2016-10-16 22:34:09 +0100

    removed routing and home, setup components for future use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,17 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule, JsonpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
-import {routing, appRoutingProviders} from './app.routing';
-import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent,
-        AboutComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
         JsonpModule,
-        routing
     ],
-    providers: [appRoutingProviders],
     bootstrap: [AppComponent]
 })
 
