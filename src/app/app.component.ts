ad405a05ea0a48b87cee8e85444281ad7f303c73
Oliver <orr2@live.com>
2016-10-22 13:09:53 +0100

    restyle, now mostly sidebar with video bg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core'
 
 import { TrainingDate } from './training-date';
+import { Event } from './event';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles.css';
@@ -17,6 +18,10 @@ export class AppComponent implements OnInit {
     trainingDates: [TrainingDate] = [
       new TrainingDate(0,14,0, 'Spiceball Park'),
       new TrainingDate(1,18,30, 'The Warriner School')
+    ];
+
+    events: [Event] = [
+      new Event('Test Event', new Date(2016, 12, 25))
     ];
 
     testTDate: TrainingDate = new TrainingDate(1,1,1,'Test');
